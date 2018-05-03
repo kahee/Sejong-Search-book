8d1a28a4c199a4a530365dc9f5ad6d57849f3b0c
kahee <yuygh131@gmail.com>
2018-05-03 23:32:59 +0900

    sentry install

---
+++ app/config/settings/local.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = []
+WSGI_APPLICATION = 'config.wsgi.local.application'
+
+INSTALLED_APPS +=[]
+
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
+MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
