435833ae4ee6d9f637c59c328828c945d471c033
kahee <yuygh131@gmail.com>
2018-05-08 11:51:30 +0900

    createus user 정보 변경

--- app/members/management/commands/createsu.py
+++ app/members/management/commands/createsu.py
@@ -1,16 +1,15 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
-
-from config import settings
 
 User = get_user_model()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username=settings.SECRETS['SUPERUSER_USERNAME']).exists():
+        if not User.objects.filter(username=settings.SUPERUSER_USERNAME).exists():
             User.objects.create_superuser(
-                username=settings.SECRETS['SUPERUSER_USERNAME'],
-                password=settings.SECRETS['SUPERUSER_PASSWORD'],
-                email=settings.SECRETS['SUPERUSER_EMAIL'],
+                username=settings.SUPERUSER_USERNAME,
+                password=settings.SUPERUSER_PASSWORD,
+                email=settings.SUPERUSER_EMAIL,
             )
