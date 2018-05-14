4e12c4a59c63d892ebe45e088e5c7517f987ea6b
kahee <yuygh131@gmail.com>
2018-05-14 14:57:55 +0900

    search_book request_json test로 수정중

--- app/books/views.py
+++ app/books/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 def keyboard(request):
+
     return JsonResponse({
         'type': 'text',
         'buttons': ['test', '2']
@@ -16,19 +17,33 @@ def keyboard(request):
 
 @csrf_exempt
 def search_book(request):
-    message = request.body.decode('utf-8')
+    message = ((request.body).decode('utf-8'))
     return_json_str = json.loads(message)
-    user_key = return_json_str['user_key']
-    content = return_json_str['content']
-
-    if content == 'test':
-        response = '응답했습니다.' + user_key
-
-    else:
-        response = '해당명령어는 아직지원하지 않습니다.'
+    return_str = return_json_str['content']
 
     return JsonResponse({
-        "message": {
-            "text": response
+        'message': {
+            'text': "you type " + return_str + "!"
+        },
+        'keyboard': {
+            'type': 'buttons',
+            'buttons': ['1', '2']
         }
     })
+
+    # message = request.body.decode('utf-8')
+    # return_json_str = json.loads(message)
+    # user_key = return_json_str['user_key']
+    # content = return_json_str['content']
+    #
+    # if content == 'test':
+    #     response = '응답했습니다.' + user_key
+    #
+    # else:
+    #     response = '해당명령어는 아직지원하지 않습니다.'+ user_key
+    #
+    # return JsonResponse({
+    #     "message": {
+    #         "text": response
+    #     }
+    # })
