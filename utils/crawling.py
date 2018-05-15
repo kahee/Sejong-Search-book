77df9835a37307e798c6983f1f43be633d2e80d8
kahee <yuygh131@gmail.com>
2018-05-15 12:48:18 +0900

    사용자 입력에 따른 keyword를 통해 도서 검색

--- utils/crawling.py
+++ utils/crawling.py
@@ -1,5 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
 
 
+def search_book_crawler(keyword):
+    url = "http://library.sejong.ac.kr/search/Search.Result.ax"
+    params = {
+        'sid': 2,
+        'q': keyword,
+        'mf': 'true',
+
+    }
+    response = requests.get(url, params)
+    print(f'url=' + response.url)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # print(soup)
 
 
+# 사용자가 입력하는 경우 '제목,출판사,저자'
+# TITL,PUBN,AUTH
 
+keyword = ''
+search = ',생능,'
+search_list = search.split(',')
+
+if search_list[0] is not '':
+    keyword = 'TITL:'+search_list[0]
+if search_list[1] is not '':
+    keyword = keyword + "|" + "PUBN:" + search_list[1]
+if search_list[2] is not '':
+    keyword = keyword + "|" + "AUTH:" + search_list[2]
+
+search_book_crawler(keyword)
