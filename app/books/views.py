72c4354200c6683fad037f2e27e08681d0d17605
kahee <yuygh131@gmail.com>
2018-05-18 12:30:17 +0900

    message view에 message_button 수정

--- app/books/views.py
+++ app/books/views.py
@@ -26,9 +26,9 @@ def message(request):
     return JsonResponse({
         'message': {
             'text': books,
+            "message_button": {
+                'label': '더 많은 검색 결과 보기',
+                'url': url,
+            }
         },
-        "message_button": {
-            'label': '더 많은 검색 결과 보기',
-            'url': url,
-        }
     })
