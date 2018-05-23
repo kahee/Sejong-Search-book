a64eff74646071f07fc2e4b85d7759c74366a053
kahee <yuygh131@gmail.com>
2018-05-23 11:31:00 +0900

    도서 위치 및 상세 정보 크롤링 함수 출력부분 수정

--- app/books/utils/crawling.py
+++ app/books/utils/crawling.py
@@ -25,19 +25,21 @@ def book_detail(book_id):
     }
 
     response = requests.get(url, params)
-    print(response.url)
     soup = BeautifulSoup(response.text, 'lxml')
     contents = soup.find('div', class_='contents')
     metaDataBody = contents.find('tbody', id='metaDataBody').find_all('td')
     items = [item.get_text(strip=True).replace(':', '') for item in metaDataBody]
     td = iter(items)
     book_info = dict(zip(td, td))
-    print(book_info)
-
     return book_info
 
 
 def book_location(book_id):
+    """
+    도서 위치 및 대출 정보를 크롤링하는 함수
+    :param book_id:
+    :return: 서가위치, 도서번호, 대출여부
+    """
     post_format = {
         'cid': book_id,
     }
@@ -45,13 +47,18 @@ def book_location(book_id):
     r = requests.post('http://library.sejong.ac.kr/search/ItemDetailSimple.axa', data=post_format)
     test = BeautifulSoup(r.content, 'html.parser')
     tbody = test.find('tbody').find_all('tr')
-    book_list = list()
+    books_list = list()
 
-    for num, item in enumerate(tbody):
-        print(item.get_text())
-        book_list.append((item.get_text()))
-        print(book_list)
-    return book_list
+    for item in tbody:
+        td = item.find_all('td')
+        book = list()
+        for num, td_item in enumerate(td):
+            if num in range(1,4):
+                book.append(td_item.get_text(strip=True))
+        books_list.append(book)
+
+    return books_list
+
 
 def books_crawler(keyword):
     """
@@ -68,42 +75,50 @@ def books_crawler(keyword):
     }
 
     response = requests.get(url, params)
-    print(response.url)
     soup = BeautifulSoup(response.text, 'lxml')
     body = soup.find('ul', class_='listType01').find_all('div', class_='body')
     # books_list = dict()
+
     books = ''
-
     for book in body:
-
         book_numbers = book.find('a', class_='title', href=True)['href']
         p = re.compile(r'javascript:search.goDetail[(](\d+)[)]')
         book_id = p.search(book_numbers).group(1)
-        book_location(book_id)
+
+        # 도서 상세 정보
         book_detail(book_id)
+        # 도서 위치 및 대출 여부
+        locations = book_location(book_id)
+
+        # 도서 위치 string으로 변환
+        books_status = ''
+        for items in locations:
+            books_status = books_status + ', '.join(str(item) for item in items) + '\n'
 
         for num, item in enumerate(book.contents):
             if num == 1:
                 book_title = item.get_text(strip=True)
             if num == 4:
                 book_info = item.strip()
-
         book_info = re.sub(r'/', '', book_info)
-        book_status = book.find('p', class_='tag').get_text(strip=True)
-        book_status = re.sub(r'\t', '', book_status)
-        book_status = re.sub(r'세종대학교 학술정보원', '', book_status)
+
+        # book_status = book.find('p', class_='tag').get_text(strip=True)
+        # book_status = re.sub(r'\t', '', book_status)
+        # book_status = re.sub(r'세종대학교 학술정보원', '', book_status)
 
         # books_list[i] = {
         #     'title': book_title,
         #     'info': book_info,
         #     'status': book_status,
         # }
-        books = books + book_title + "\n" + book_info + "\n" + book_status + "\n" + "---------" + "\n"
+
+        books = books + book_title + "\n" + book_info + "\n" + books_status + "---------" + "\n"
 
     if not books:
         books = '검색하신 결과가 없습니다.'
         url = None
         return books, url
+    print(books)
     return books, response.url
 
 
@@ -148,8 +163,7 @@ def search_book(keyword):
 
 # # 사용자가 입력하는 경우 '제목,출판사,저자'
 # # TITL,PUBN,AUTH
-result = search_book('말의품격')
-print(result)
-
+# result = search_book('컴퓨터구조')
+# print(result)
 # book_detail(1578922)
 # book_location(1578922)
