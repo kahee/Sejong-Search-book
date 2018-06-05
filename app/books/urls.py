ce6261d4a6e8c0da9f881f551911b86f2a0d5431
kahee <yuygh131@gmail.com>
2018-06-05 14:24:37 +0900

    book url 수정

--- app/books/urls.py
+++ app/books/urls.py
@@ -4,6 +4,6 @@ from books import views
 urlpatterns = [
     path('keyboard/', views.keyboard, name='keyboard'),
     path('message', views.message, name='message'),
-    path('friend/', views.plus_friend, name='plus-friend'),
+    path('friend', views.plus_friend, name='plus-friend'),
     path('friend/<str:user_key>/', views.delete_friend, name='delete-firend'),
 ]
