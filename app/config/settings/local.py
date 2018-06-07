8d5a1a523f9bab7775813c36ae9908ee205c8e36
kahee <yuygh131@gmail.com>
2018-06-07 13:06:07 +0900

    local DB  postgresql로 변경

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -10,9 +10,9 @@ INSTALLED_APPS += [
     'django_extensions',
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
