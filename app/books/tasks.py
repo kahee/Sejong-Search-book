29248b20b638e38971fcd0b3300a36fc794261c9
kahee <yuygh131@gmail.com>
2018-06-27 12:18:32 +0900

    키워드 입력시 책의 상세 정보, 위치 정보를 celery task로 처리 1. book_detail_save() : 책의 상세 정보를 저장 2. book_location_save() : 책 위치 정보를 저장

--- app/books/tasks.py
+++ app/books/tasks.py
@@ -1,15 +1,38 @@
-from .models import Book
+import time
+
+from .models import BookLocation, Book
 from .utils import get_book_detail
 from config.celery import app
 
 
-@app.task(bind=True)
-def book_detail_save(self):
-    books = Book.objects.all()
+# @app.task(bind=True)
+# def book_detail_save(self):
+#     books = Book.objects.all()
+#
+#     for book in books:
+#         print(book.book_id)
+#         if not book.ISBN:
+#             get_book_detail(book.book_id)
+#             print(f'{book.book_id}가 저장되었습니다.')
+#     return f'끝'
 
-    for book in books:
-        print(book.book_id)
-        if not book.ISBN:
-            get_book_detail(book.book_id)
-            print(f'{book.book_id}가 저장되었습니다.')
-    return f'끝'
+
+@app.task()
+def book_detail_save(book_id):
+    get_book_detail(book_id)
+    return f'{book_id}가 저장되었습니다.'
+
+
+@app.task()
+def book_location_save(book_id, register_id, location, book_code):
+    time.sleep(3)
+    book_location, _ = BookLocation.objects.update_or_create(
+        register_id=register_id,
+        defaults={
+            'location': location,
+            'book_code': book_code,
+            'book': Book.objects.get(book_id=book_id),
+        }
+    )
+
+    return f'{book_id}/{register_id}가 저장되었습니다.'
