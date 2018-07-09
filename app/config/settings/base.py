f3f8d11586789706566ab0bb91c90f7fb4c76721
kahee <yuygh131@gmail.com>
2018-07-09 12:26:48 +0900

    template dir  추가

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -18,7 +18,7 @@ from djs import import_secrets
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ROOT_DIR = os.path.dirname(BASE_DIR)
-
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 
 SECRETS_MODULES = {
     # Module's full path stri1ng
@@ -71,7 +71,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
