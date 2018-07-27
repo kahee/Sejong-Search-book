04fba16982d373b0a521c862e2c08abc6eeb8ff1
kahee <yuygh131@gmail.com>
2018-07-27 11:43:28 +0900

    add static files

---
+++ app/static/js/common.js
@@ -0,0 +1,48 @@
+
+var introTagA =  document.getElementById("introTagA")
+var introVideoTagA = document.getElementById("introVideoTagA")
+var aboutMeTagA = document.getElementById("aboutMeTagA")
+
+var hamburgerBar = document.getElementsByClassName("hamburgerBar");
+var nav =   document.getElementsByTagName("nav");
+
+
+function closeNav() {
+  if (nav.classList) {
+    nav.classList.remove("open");
+    nav.classList.toggle("close");
+  }
+}
+
+introTagA.addEventListener("click", function() {
+  $("nav").toggleClass("close");
+  $([document.documentElement, document.body]).animate({
+    scrollTop: 0
+}, 1000);
+})
+
+introVideoTagA.addEventListener("click", function() {
+  $("nav").toggleClass("close");
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $('#introductionVideo').offset().top-40
+}, 1000);
+})
+
+ aboutMeTagA.addEventListener("click", function() {
+  $("nav").toggleClass("close");
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $('#aboutMe').offset().top
+}, 1000);
+})
+
+
+$(".hamburgerBar").click(function(){
+  $("nav").removeClass("close");
+  $("nav").toggleClass("open");
+});
+
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
