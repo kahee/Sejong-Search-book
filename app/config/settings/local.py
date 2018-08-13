7b5fc93e2d9694c0b13523c984f4faef48283339
kahee <yuygh131@gmail.com>
2018-08-13 15:31:25 +0900

    setting local/production django log 파일 생성 추가 및 주석 정리

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -10,8 +10,52 @@ INSTALLED_APPS += [
     'django_extensions',
 ]
 
+# Celery
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+# Log
+LOG_DIR = os.path.join(ROOT_DIR, '.log')
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            'format': '[%(asctime)s] %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'file_error': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'formatter': 'django.server',
+            'backupCount': 10,
+            'filename': os.path.join(LOG_DIR, 'error.log'),
+            'maxBytes': 10485760,
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file_error'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
+
 #
 # DATABASES = {
 #     'default': {
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -21,6 +21,7 @@ INSTALLED_APPS += [
 # SECURE_SSL_REDIRECT = True
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Celery
 CELERY_BROKER_URL = 'redis://' + AWS_ELASTIC_CACHE
 CELERY_RESULT_BACKEND = 'redis://' + AWS_ELASTIC_CACHE
 
@@ -29,7 +30,56 @@ DEFAULT_FILE_STORAGE = 'config.storage.DefaultFilesStorage'
 # # Static files(collectstatic) 을 위한 스토리지
 STATICFILES_STORAGE = 'config.storage.StaticFilesStorage'
 
+# Log
+# /var/log/django 디렉토리가 존재하면 LOG_DIR로 그대로 사용
+# 없으면 ROOT_DIR/.log디렉토리를 사용 (없으면 생성)
+LOG_DIR = '/var/log/django'
+if not os.path.exists(LOG_DIR):
+    LOG_DIR = os.path.join(ROOT_DIR, '.log')
+    os.makedirs(LOG_DIR, exist_ok=True)
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            'format': '[%(asctime)s] %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'file_error': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'formatter': 'django.server',
+            'backupCount': 10,
+            'filename': os.path.join(LOG_DIR, 'error.log'),
+            'maxBytes': 10485760,
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file_error'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    }
+}
+
+
+# Elastic load Balance HealthCheck 설정
 def is_ec2_linux():
     """Detect if we are running on an EC2 Linux Instance
        See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
