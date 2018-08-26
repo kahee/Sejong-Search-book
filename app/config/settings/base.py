12b2197c2ecfe95f0b758dccf4f11f0c6570df5e
kahee <yuygh131@gmail.com>
2018-08-27 00:44:23 +0900

    수정 : base.py 주석 정리

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -11,45 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import json
 import os
-import requests
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-
-SECRETS_MODULES = {
-    # Module's full path string
-    'raven': 'raven',
-    # Python module object
-    'requests': requests,
-}
-
-# SECRETS
-# if 'TRAVIS_PULL_REQUEST' in os.environ:
-#     print(os.environ)
-#     if os.environ['TRAVIS_PULL_REQUEST'] is False:
-#         print('pr 요청이 아닌 경우')
-#         print(os.environ['TRAVIS_PULL_REQUEST'])
-#         SECRETS_DIR = os.path.join(ROOT_DIR, '.secrets')
-#         SECRETS_BASE = os.path.join(SECRETS_DIR, 'base.json')
-#         SECRETS = json.loads(open(SECRETS_BASE, 'rt').read())
-#         SECRET_KEY = SECRETS['SECRET_KEY']
-#
-#         # AWS
-#         AWS_ACCESS_KEY_ID = SECRETS['AWS_ACCESS_KEY_ID']
-#         AWS_SECRET_ACCESS_KEY = SECRETS['AWS_SECRET_ACCESS_KEY']
-#         AWS_STORAGE_BUCKET_NAME = SECRETS['AWS_STORAGE_BUCKET_NAME']
-#         AWS_S3_REGION_NAME = SECRETS['AWS_S3_REGION_NAME']
-#         AWS_S3_SIGNATURE_VERSION = SECRETS['AWS_S3_SIGNATURE_VERSION']
-#         AWS_DEFAULT_ACL = SECRETS['AWS_DEFAULT_ACL']
-#         AWS_ELASTIC_CACHE = SECRETS['AWS_ELASTIC_CACHE']
-#
-#         # CREATE SUPER USER
-#         SUPERUSER_USERNAME = SECRETS['SUPERUSER_USERNAME']
-#         SUPERUSER_PASSWORD = SECRETS['SUPERUSER_PASSWORD']
-#         SUPERUSER_EMAIL = SECRETS['SUPERUSER_EMAIL']
-print('base.py')
 print(os.environ)
 if os.environ.get('TRAVIS'):
     print(os.environ.get('TRAVIS_PULL_REQUEST'))
